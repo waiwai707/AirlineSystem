bc90d7fb2cd84b4e6a560ce72c271378cedbae03
waiwai707 <waiwai07077@gmail.com>
2023-12-08 21:52:27 +0630

    change URL and Setting.py

--- airlinesystem/airlinesystem/settings.py
+++ airlinesystem/airlinesystem/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'airlinesystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'airlinemyanmar',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- airlinesystem/airlinesystem/urls.py
+++ airlinesystem/airlinesystem/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portal',include('bcahub.urls')),
+    path('portal/',include('bcahub.urls')),
 ]
